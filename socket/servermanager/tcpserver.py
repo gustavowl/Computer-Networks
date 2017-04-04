86f60d9ac205bf3b254e48678310f804d5a59cd4
Gustavo <gustavoalvesbezerra@gmail.com>
2017-04-04 17:51:10 -0300

    Implements request processing from multiple users

--- socket/servermanager/tcpserver.py
+++ socket/servermanager/tcpserver.py
@@ -1,13 +1,26 @@
 from socket import *
 import _thread
 
-def first_thread():
-	for i in range(100):
-		print("THREAD 1")
+#this function will be responsible for keep listening to the
+#input comming from the client
+def persistent_socket_connection(con_socket):
+	#reads message sent from client
+	receivedMessage = conSocket.recvfrom(2112)
 
-def second_thread():
-	for i in range(100):
-		print("\tTHREAD 2")
+	#if receivedMessage[0] == "" then connection was closed
+	while receivedMessage[0].decode() != "":
+		print(receivedMessage[0].decode()) #debugging reasons
+		#computes desired algorithm for client
+		answerMessage = receivedMessage[0].decode().upper()
+		#send it back to client
+		con_socket.send(answerMessage.encode())
+		#waits for new request or end of connection
+		receivedMessage = con_socket.recvfrom(2112)
+	#reference to 1994's World's cup final
+	#it means "It is over"
+	print("-------------CABOU!!!")
+
+#---------------------------------------------------------------
 
 serverPort = 9001 #socket will listen to port 12112
 serverSocket = socket(AF_INET, SOCK_STREAM) #IPv4, UDP
@@ -17,19 +30,13 @@ serverSocket.bind(('', serverPort))
 serverSocket.listen(2)
 print("The server is ready to receive")
 
-_thread.start_new_thread( first_thread, () )
-_thread.start_new_thread( second_thread, () )
-
-#infinite loop (socket will always listen)
+#infinite loop (this threads' socket will always listen for new connections)
 while True:
+	#starts TCP connection required by client
 	conSocket, addr = serverSocket.accept()
-	#stores data sent from client and client's adress and port, respectively
-	connection_closed = False
-	receivedMessage = conSocket.recvfrom(2112)
-	while receivedMessage[0].decode() != "":
-		print(receivedMessage[0].decode())
-		#applies changes to received message
-		answerMessage = receivedMessage[0].decode().upper()
-		conSocket.send(answerMessage.encode())
-		receivedMessage = conSocket.recvfrom(2112)
-	print("stop it please")
+
+	#creates thread that will run the connection socket behavior while
+	#the current thread will be responsible for listening to new connections.
+	#The new thread will start running at persistent_socket_connection
+	#function that receives a socket as parameter
+	_thread.start_new_thread(persistent_socket_connection, (conSocket, ))
